5c50bffce9ab125cb50764f156629a6eebf07d54
Hugo Lamarche <lamarchehugo@gmail.com>
2021-08-25 10:19:19 -0400

    Remove sample function and move implementation of the API to cpp

--- NativePlugin_Asteroids/AsteroidsCPP/src/AsteroidsAPI.cpp
+++ NativePlugin_Asteroids/AsteroidsCPP/src/AsteroidsAPI.cpp
@@ -6,14 +6,34 @@
 
 namespace AsteroidsCPP
 {
-
-	const char* HelloWorld()
+	ASTEROID_EXPORT void* AllocateGamePtr(float shipControlSpeed, float shipControlRotationSpeed, float shipMaxSpeed, std::uint32_t asteroidTemplatesCount, std::uint32_t maxAsteroidsCount, Vec2 viewportSize)
 	{
-		return "Hello World!";
+		return new AsteroidsCPP::Game(shipControlSpeed, shipControlRotationSpeed, shipMaxSpeed, asteroidTemplatesCount, maxAsteroidsCount, viewportSize);
 	}
 
-	std::uint32_t AddNumbers(std::uint32_t a, std::uint32_t b)
+	ASTEROID_EXPORT void DetroyGamePtr(void* gamePtr)
 	{
-		return a + b;
+		if (gamePtr)
+			delete gamePtr;
+	}
+
+	ASTEROID_EXPORT std::uint32_t GetAsteroidsCount(void* gamePtr)
+	{
+		return static_cast<AsteroidsCPP::Game*>(gamePtr)->GetAsteroidsCount();
+	}
+
+	ASTEROID_EXPORT void Update(void* gamePtr, AsteroidsCPP::Game::KeyState keyState, float deltaTime)
+	{
+		static_cast<AsteroidsCPP::Game*>(gamePtr)->Update(keyState, deltaTime);
+	}
+
+	ASTEROID_EXPORT void GetAsteroidsPositions(void* gamePtr, Vec2** positions)
+	{
+		*positions = static_cast<AsteroidsCPP::Game*>(gamePtr)->GetAsteroidsPositions();
+	}
+
+	ASTEROID_EXPORT AsteroidsCPP::Vec3 GetShipPosRot(void* gamePtr)
+	{
+		return static_cast<AsteroidsCPP::Game*>(gamePtr)->GetShipPosRot();
 	}
 }
